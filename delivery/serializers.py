f3b1d4ead242b8fc38730a63440178c1dc9cd225
TrapiFox <leigst@gmail.com>
2024-12-04 17:03:30 +0300

    валидация полей

--- delivery/serializers.py
+++ delivery/serializers.py
@@ -1,28 +1,70 @@
+import re
 from rest_framework import serializers
 from .models import User, UserAddress, Courier, Restaurant, RestaurantGroup, RestaurantDish, RestaurantAttribute, Order, OrderDish, OrderAttribute, Ticket
 
+# ТРЕТИЙ СПОСОБ ВАЛИДАЦИИ
+def is_login_unique(value):
+    if User.objects.filter(login=value).exists():
+        raise serializers.ValidationError("Данный логин уже занят")
+    return value
+
+
 class UserSerializer(serializers.ModelSerializer):
+    login = serializers.CharField(validators=[is_login_unique])
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ['login', 'password', 'email', 'first_name', 'last_name']
+    
+    # ПЕРВЫЙ СПОСОБ ВАЛИДАЦИИ
+    def validate(self, values):
+        if not re.match(r'^[А-Яа-я\s]+$', values['first_name']):
+            raise serializers.ValidationError("Имя пользователя может содержать только символы кириллицы")
+        
+        if not re.match(r'^[А-Яа-я\s]+$', values['last_name']):
+            raise serializers.ValidationError("Фамилия пользователя может содержать только символы кириллицы")
+        return values
+    
+    # ВТОРОЙ СПОСОБ ВАЛИДАЦИИ
+    def validate_password(self, value):
+        if len(value) != 64:
+            raise serializers.ValidationError("Неправильный хэш пароля")
+        if not re.match(r'^[0-9a-f]+$', value):
+            raise serializers.ValidationError("Неправильный хэш пароля")
+    
 
 
 class UserAddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAddress
         fields = "__all__"
+    
+    def validate(self, values):
+        if not re.match(r'^[А-Яа-я\s]+$', values['address']):
+            raise serializers.ValidationError("Адрес может содержать только символы кириллицы")
 
 
 class CourierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Courier
         fields = "__all__"
+    
+    def validate(self, values):
+        if not re.match(r'^[А-Яа-я\s]+$', values['first_name']):
+            raise serializers.ValidationError("Имя пользователя может содержать только символы кириллицы")
+        
+        if not re.match(r'^[А-Яа-я\s]+$', values['last_name']):
+            raise serializers.ValidationError("Фамилия пользователя может содержать только символы кириллицы")
+        return values
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = "__all__"
+    
+    def validate(self, values):
+        if not re.match(r'^[А-Яа-я0-9\s]+$', values['title']):
+            raise serializers.ValidationError("Название может содержать только символы кириллицы")
 
 
 class RestaurantGroupSerializer(serializers.ModelSerializer):
