96290070b2a6743c1e71520d557f8c541cfb2508
TrapiFox <leigst@gmail.com>
2024-12-04 14:25:24 +0300

    тренировка подключения вьюшки к апи на тикетах

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
+from delivery import views
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register('tickets', views.TicketViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
