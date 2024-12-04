20aea7bf3cb0aa8c598259b9cbf023a342717cd4
TrapiFox <leigst@gmail.com>
2024-12-04 12:20:23 +0300

    full reset for restart

--- coursework/main/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Users
-
-class UsersSerialuzer(serializers.ModelSerializer):
-    class Meta:
-        model = Users
-        fields = ('role', 'first_name', 'last_name')
--- coursework/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import Users
-from .serializers import UsersSerialuzer
-
-class APIUsersView(generics.ListAPIView):
-    queryset = Users.objects.all()
-    serializer_class = UsersSerialuzer
