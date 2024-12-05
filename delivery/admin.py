9c61df1beb8c92c876231eab2e7b1a98e0040b9d
TrapiFox <leigst@gmail.com>
2024-12-05 20:15:55 +0300

    5 атрибутов админ-панели

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,46 +1,67 @@
 from django.contrib import admin
+from import_export.admin import ImportExportActionModelAdmin
 from .models import User, UserAddress, Courier, Restaurant, RestaurantGroup, RestaurantDish, RestaurantAttribute, Order, OrderDish, OrderAttribute, Ticket
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['login', 'first_name', 'last_name']
+    search_fields = ['login']
+    fields = ['last_name', 'first_name']
 
 @admin.register(UserAddress)
-class UserAdmin(admin.ModelAdmin):
+class UserAddressAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['id_user', 'address']
+    list_filter = ['id_user']
+    search_fields = ['address']
 
 @admin.register(Courier)
-class UserAdmin(admin.ModelAdmin):
+class CourierAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['last_name', 'first_name']
+    search_fields = ['last_name']
 
 @admin.register(Restaurant)
-class UserAdmin(admin.ModelAdmin):
+class RestaurantAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['title']
+    search_fields = ['title']
 
 @admin.register(RestaurantGroup)
-class UserAdmin(admin.ModelAdmin):
+class RestaurantGroupAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['title', 'id_restaurant']
+    list_editable = ['id_restaurant']
+    list_filter = ['id_restaurant']
+    search_fields = ['title']
 
 @admin.register(RestaurantDish)
-class UserAdmin(admin.ModelAdmin):
+class RestaurantDishAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['title', 'id_group']
+    list_editable = ['id_group']
+    list_filter = ['id_group']
+    search_fields = ['title']
 
 @admin.register(RestaurantAttribute)
-class UserAdmin(admin.ModelAdmin):
+class RestaurantAttributeAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['title', 'id_dish']
+    list_editable = ['id_dish']
+    list_filter = ['id_dish']
+    search_fields = ['title']
 
 @admin.register(Order)
-class UserAdmin(admin.ModelAdmin):
+class OrderAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['id', 'id_user']
+    search_fields = ['id_user']
 
 @admin.register(OrderDish)
-class UserAdmin(admin.ModelAdmin):
+class OrderDishAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['id', 'id_order']
+    search_fields = ['id_order']
 
 @admin.register(OrderAttribute)
-class UserAdmin(admin.ModelAdmin):
+class OrderAttributeAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['id', 'id_dish']
+    search_fields = ['id_dish']
 
 @admin.register(Ticket)
-class UserAdmin(admin.ModelAdmin):
+class TicketAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
     list_display = ['id_order']
+    search_fields = ['id_order']
+    list_display_links = ['id_order']
